2e3e918cf7540a1d806395fbab5633962985f96b
Flavia Mendes <flaviamendesdasilva99@gmail.com>
2023-05-20 21:48:30 -0300

    adicionando pergunta  fechada e seu texto tachado com a cor correta correspondente ao seu status e um ícone.

--- src/components/Flashcards.jsx
+++ src/components/Flashcards.jsx
@@ -2,56 +2,79 @@ import { useState } from "react"
 import styled from "styled-components"
 import seta from "../assets/seta_play.png"
 import setaVirada from "../assets/seta_virar.png"
+import certo from "../assets/icone_certo.png"
+import quase from "../assets/icone_quase.png"
+import errado from "../assets/icone_erro.png"
+import {verde} from "../css/colors"
+import {vermelho} from "../css/colors"
+import {laranja} from "../css/colors" 
+import {cinza} from "../css/colors"
+import {rosa} from "../css/colors"
+
 
 export default function Flashcards({ index, card }) {
     const [inicio, setInicio] = useState(false)
     const [virada, setVirada] = useState(false)
     const [perguntaFinalizada, setPerguntaFinalizada] = useState(false)
-
-    
-function mostrarPergunta() {
-    if (!perguntaFinalizada) {
-        setInicio(true)}
-    
-}
-
-function mostrarResposta() {
-    setVirada(true)
-}
-
-function perguntaRespondida() {
-setInicio(false)
-setPerguntaFinalizada(true)
-}
+    const [statusDaPergunta, setStatusDaPergunta] = useState('nao respondido')
 
 
-           switch (true) {
-  case !inicio:
-    return (
-      <Cards>
-        <p>Pergunta {index + 1}</p>
-        <img src={seta} onClick={mostrarPergunta} />
-      </Cards>
-    );
-  case !virada:
-    return (
-      <CardAberto>
-        {card.question}
-        <img src={setaVirada} onClick={mostrarResposta} />
-      </CardAberto>
-    );
-  default:
-    return (
-      <CardAberto>
-        {card.answer}
-        <Botoes>
-          <button onClick={perguntaRespondida}>Não Lembrei</button>
-          <button onClick={perguntaRespondida}>Quase não lembrei</button>
-          <button onClick={perguntaRespondida}>Zap!</button>
-        </Botoes>
-      </CardAberto>
-    );
-}
+    function mostrarPergunta() {
+        {!perguntaFinalizada ? setInicio(true) : null}
+
+    }
+
+    function mostrarResposta() {
+        setVirada(true)
+    }
+
+    function perguntaRespondida(status) {
+        setInicio(false)
+        setPerguntaFinalizada(true)
+        setStatusDaPergunta(status)
+    }
+
+    function seletor() {
+        switch (statusDaPergunta) {
+            case 'errado':
+                return errado
+            case 'quase':
+                return quase
+            case 'certo':
+                return certo
+            default:
+                return seta
+
+        }
+    }
+
+    switch (true) {
+        case !inicio:
+            return (
+                <Cards status={statusDaPergunta}>
+                    <p>Pergunta {index + 1}</p>
+                    <img src={seletor()} onClick={mostrarPergunta} />
+                </Cards>
+            );
+        case !virada:
+            return (
+                <CardAberto>
+                    {card.question}
+                    <img src={setaVirada} onClick={mostrarResposta} />
+                </CardAberto>
+            );
+        default:
+            return (
+                <CardAberto>
+                    {card.answer}
+                    <Botoes>
+                        <button onClick={() => perguntaRespondida('errado')}>Não Lembrei</button>
+                        <button onClick={() => perguntaRespondida('quase')}>Quase não lembrei</button>
+                        <button onClick={() => perguntaRespondida('certo')}>Zap!</button>
+                    </Botoes>
+                </CardAberto>
+            );
+    }
 
 }
 
@@ -68,13 +91,22 @@ const Cards = styled.div`
   justify-content: space-between;
   padding-right: 15px;
   
+  
   p {
     font-family: 'Recursive';
     font-style: normal;
     font-weight: 700;
     font-size: 16px; 
-    color: #333333;
-   }
+    color: ${props => {
+        return props.status === 'certo'
+          ? verde
+          : props.status === 'errado'
+          ? vermelho
+          : props.status === 'quase'
+          ? laranja
+          : cinza;
+      }}
+      }
   `
 
 const CardAberto = styled.div`
@@ -85,7 +117,6 @@ font-style: normal;
 font-weight: 400;
 font-size: 18px;
 line-height: 22px;
-color: #333333;
 background: #FFFFD5;
 box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
 border-radius: 5px;
@@ -93,6 +124,7 @@ margin-bottom: 25px;
 display: flex;
 justify-content: space-between;
 flex-direction: column;
+
 
 img {
     width: 30px;
